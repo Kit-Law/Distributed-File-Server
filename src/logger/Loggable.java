0cae7140c5fcaa8675ff45287a9f029b5637e461
Kit <34525535+kit-law@users.noreply.github.com>
2021-05-03 15:03:19 +0100

    -Imported the new files supplied by the uni.

--- src/logger/Loggable.java
+++
@@ -1,9 +0,0 @@
-package logger;
-
-/**
- * An interface for marking a class as being able to output a log file.
- *
- * @author Christopher Lawrence
- * */
-public interface Loggable
-{  }
--- src/logger/Logger.java
+++ src/logger/Logger.java
@@ -1,111 +1,54 @@
 package logger;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+import java.io.PrintStream;
+import java.net.Socket;
 
-/**
- * A logging class that outputs to a logging file, if the log level is great enough,
- * and to the console in different colours. The log file has the format:
- *
- * [Current system time (s)] [Log level] [msg]
- *
- * @author Christopher Lawrence
- */
-public class Logger
-{
-	/** This holds the path to the output log file. */
-	private static Path logFile = null;
-	/** This holds the level of the log that needs to be reached
-	 * before outputting to the log file. */
-	private static LogLevel outputLevel = LogLevel.WARNING;
-	
-	/** A inner class to wrap logs with LogLevel.INFO. */
-	public static class info
-	{
-		public static void log(String msg)
-		{
-			Logger.logToConsole(msg, LogLevel.INFO);
-			Logger.logToFile(msg, LogLevel.INFO);
-		}
+public abstract class Logger {
+
+	public enum LoggingType {
+		NO_LOG, // no log at all
+		ON_TERMINAL_ONLY, // log to System.out only 
+		ON_FILE_ONLY, // log to file only
+		ON_FILE_AND_TERMINAL // log to both System.out and file
 	}
 	
-	/** A inner class to wrap logs with LogLevel.WARNING. */
-	public static class wrn
-	{
-		public static void log(String msg)
-		{
-			Logger.logToConsole(msg, LogLevel.WARNING);
-			Logger.logToFile(msg, LogLevel.WARNING);
-		}
+	protected final LoggingType loggingType;
+	protected PrintStream ps;
+	
+	protected Logger(LoggingType loggingType) {
+		this.loggingType = loggingType;
 	}
 	
-	/** A inner class to wrap logs with LogLevel.ERROR. */
-	public static class err
-	{
-		public static void log(String msg)
-		{
-			Logger.logToConsole(msg, LogLevel.ERROR);
-			Logger.logToFile(msg, LogLevel.ERROR);
-		}
+	protected abstract String getLogFileSuffix();
+	
+	protected synchronized PrintStream getPrintStream() throws IOException {
+		if (ps == null)
+			ps = new PrintStream(getLogFileSuffix() + "_" + System.currentTimeMillis() + ".log");
+		return ps;
 	}
 	
-	/**
-	 * Outputs a log message and respective log level to the console.
-	 *
-	 * @param msg The message to log.
-	 * @param level The LogLevel of the message.
-	 * */
-	private static void logToConsole(String msg, LogLevel level)
-	{
-		System.out.println(
-				level.getColour() +
-				(LogLevel.WARNING.satisfies(level) ? level.name() + ": " : "") +
-				msg + "\u001B[0m");
+	protected boolean logToFile() {
+		return loggingType == LoggingType.ON_FILE_ONLY || loggingType == LoggingType.ON_FILE_AND_TERMINAL;
 	}
 	
-	/**
-	* Outputs a log message and respective log level to the output file.
-	*
-	* @param msg The message to log.
-	* @param level The LogLevel of the message.
-	*/
-	private static void logToFile(String msg, LogLevel level)
-	{
-		//Checks if the log is worth outputting to a file
-		if (!outputLevel.satisfies(level))
-			return;
-			
-		try
-		{
-			//Appends the output file
-			Files.write(
-					logFile,
-					(System.currentTimeMillis() + " " + level.name() + " " + msg + "\n").getBytes(),
-					StandardOpenOption.APPEND);
-		}
-		catch (Exception e) { e.printStackTrace(); }
+	protected boolean logToTerminal() {
+		return loggingType == LoggingType.ON_TERMINAL_ONLY || loggingType == LoggingType.ON_FILE_AND_TERMINAL;
 	}
 	
-	/**
-	 * Sets the current log file for the running program.
-	 *
-	 * @param obj The main object that is going to be used for logging.
-	 */
-	public static void setLogFile(Loggable obj)
-	{
-		logFile = Paths.get("./" + obj.toString() + ".log");
-		
-		try { Files.createFile(logFile); }
-		catch (IOException e) { e.printStackTrace(); }
+	protected void log(String message) {
+		if (logToFile())
+			try { getPrintStream().println(message); } catch(Exception e) { e.printStackTrace(); }
+		if (logToTerminal())
+			System.out.println(message);
 	}
 	
-	/**
-	 * Sets the log level for the output file.
-	 *
-	 * @param logLevel The level the needed to reach to output to the log file.
-	 */
-	public static void setLogLevel(LogLevel logLevel) { outputLevel = logLevel; }
+	public void messageSent(Socket socket, String message) {
+		log("[" + socket.getLocalPort() + "->" + socket.getPort() + "] " + message);
+	}
+	
+	public void messageReceived(Socket socket, String message) {
+		log("[" + socket.getLocalPort() + "<-" + socket.getPort() + "] " + message);
+	}
+	
 }
